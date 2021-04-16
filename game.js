ee6a67a5a1751c6c4fdd2f552d5ee4dcd49041d2
Ali Furkan YILMAZ <afy@mail.com.tr>
2021-04-16 05:28:19 +0300

    Update game.js

--- game.js
+++ game.js
@@ -3,23 +3,11 @@ exports.startGame = function (
   socket,
   room,
   lobbyId,
-  tabooCards,
+  faulCards,
   roomSockets,
   gameInterval,
   lobby
 ) {
-  //game variables
-  // let usedWords = [];
-  // let overAllTurn = 0;
-  // let team1Members = [];
-  // let team2Members = [];
-  // let team1Turn = 0;
-  // let team2Turn = 0;
-  //start the game
-  // io.in(lobbyId).emit("enough-players");
-  // io.in(lobbyId).emit("start-game");
-
-  //game-entered
   socket.on("game-entered", () => {
     if (!room.isGameOn) {
       room.timer = room.totalTimer;
@@ -38,17 +26,15 @@ exports.startGame = function (
       if (room.teamTurn === myTeam) {
         roomSockets[socket.id].emit("role-finder");
       } else {
-        roomSockets[socket.id].emit("role-controller", tabooCards[cardToSend]);
+        roomSockets[socket.id].emit("role-controller", faulCards[cardToSend]);
       }
       io.in(lobbyId).emit("room-update", room);
     }
 
-    //finally
     socket.emit("room-start", room);
   });
 
   function toggleTimer() {
-    //console.log("intervaldeyim");
     io.in(lobbyId).emit("time-update", room.timer);
     room.timer--;
     if (room.timer === -1) {
@@ -60,7 +46,6 @@ exports.startGame = function (
           (room[`overAllTurn${room.teamTurn}`] + 1) %
           room[`team${room.teamTurn}`].length;
         room.teamTurn = room.teamTurn === 1 ? 2 : 1;
-        //console.log("en son odanin hali", room);
         io.in(lobbyId).emit("room-update", room);
         io.in(lobbyId).emit("turn-ended");
         io.in(lobbyId).emit("play-sound", "turn-ended");
@@ -71,27 +56,13 @@ exports.startGame = function (
 
   socket.on("toggle-time", () => {
     io.in(lobbyId).emit("play-sound", "toggle-timer");
-    //console.log("toggle timedayim");
-    //console.log("TOOGLE TIME", room);
 
     room.isTimerOn = !room.isTimerOn;
-    //console.log("TOGGLE TIME", room.isTimerOn);
-
-    //console.log("intervalden once");
-    //let myInterval;
-    //room.gameInterval = 1;
     if (room.isTimerOn) {
-      //let tmpTimer;
       gameInterval.myInterval = setInterval(toggleTimer, 1000);
-      //gameInterval = tmpTimer[Symbol.toPrimitive]();
-      //console.log("game interval opened", gameInterval);
     } else {
-      //console.log("game interval before closed", gameInterval);
       clearInterval(gameInterval.myInterval);
-      //gameInterval = null;
-      //console.log("game interval after closed", gameInterval);
     }
-    //console.log(socket.id, gameInterval.myInterval);
     io.in(lobbyId).emit("room-update", room);
   });
 
@@ -108,7 +79,7 @@ exports.startGame = function (
       room[`team${currentTeam}Score`]++;
       room.lastOperation = 1;
     } else if (action === "faul") {
-      io.in(lobbyId).emit("play-sound", "taboo");
+      io.in(lobbyId).emit("play-sound", "faul");
       room[`team${currentTeam}Score`]++;
       room.lastOperation = 1;
       [otherTeam, currentTeam] = [currentTeam, otherTeam];
@@ -123,8 +94,6 @@ exports.startGame = function (
       randomCard = defaultCard;
       room.lastCard = room.usedCards[room.usedCards.length - 1];
     }
-    //console.dir(io.sockets);
-    //console.log("new card", socket.id, currentTeam);
     if (room.lastCard !== -1 && defaultCard === 0) {
       randomCard = room.lastCard;
       room.lastCard = -1;
@@ -143,84 +112,58 @@ exports.startGame = function (
           room[`team${currentTeam}`][room[`overAllTurn${currentTeam}`]]
         )[0]
       ) {
-        //socket.emit("role-teller", tabooCards[randomCard]);
         roomSockets[Object.keys(player)[0]].emit(
           "role-teller",
-          tabooCards[randomCard]
+          faulCards[randomCard]
         );
       } else {
-        //io.to(Object.keys(player)[0]).emit("role-finder");
         roomSockets[Object.keys(player)[0]].emit("role-finder");
       }
     });
 
-    //console.log("otherr team", otherTeam);
     room[`team${otherTeam}`].forEach((player) => {
-      // io.to(Object.keys(player)[0]).emit(
-      //   "role-controller",
-      //   tabooCards[randomCard]
-      // );
       roomSockets[Object.keys(player)[0]].emit(
         "role-controller",
-        tabooCards[randomCard]
+        faulCards[randomCard]
       );
     });
     io.in(lobbyId).emit("room-update", room);
     io.in(lobbyId).emit("lobby", lobby);
   });
 
-  //leave the game
-
   function deletePlayer(currentTeam) {
-    //socket.removeAllListeners();
     if (!room) {
-      //socket.emit("bad-url");
       return;
     }
-    //io.in(lobbyId).emit("play-sound", "logout-game");
-    //console.log("im deleting from game", socket.id);
     room.memberCount = room.memberCount - 1;
     room.readyMemberCount -= 1;
-    //lobby.memberCount -= 1;
-    //lobby.readyMemberCount -= 1;
     let deletingIndex = room[`team${currentTeam}`].findIndex((player) => {
       return Object.keys(player)[0] === socket.id;
-      //console.log(player);
     });
-    //console.log(deletingIndex);
     if (deletingIndex === -1) {
       return;
     }
-    //console.log("before room", room.team1, room.team2);
     room[`team${currentTeam}`].splice(deletingIndex, 1);
-    //lobby[`team${currentTeam}`].splice(deletingIndex, 1);
-    //delete roomSockets[socket.id];
     room.roomLeader = Object.keys(roomSockets)[0];
-    //lobby.roomLeader = Object.keys(roomSockets)[0];
     io.in(lobbyId).emit("room-update", room);
-    //io.in(lobbyId).emit("lobby", lobby);
     if (
       currentTeam === room.teamTurn &&
       room[`overAllTurn${currentTeam}`] === deletingIndex
     ) {
       room[`overAllTurn${currentTeam}`] =
         room[`overAllTurn${currentTeam}`] % room[`team${currentTeam}`].length;
-      //console.log("new turn", room[`overAllTurn${currentTeam}`]);
 
       io.in(lobbyId).emit("room-update", room);
       io.in(lobbyId).emit("turn-ended");
     }
 
     if (room.team1.length < 2 || room.team2.length < 2) {
-      //TODO: stop the game
       room.isGameInterrupted = true;
       room.isTimerOn = false;
       clearInterval(gameInterval.myInterval);
       io.in(lobbyId).emit("room-update", room);
     }
-    //console.log("after room", room.team1, room.team2);
   }
 
   socket.on("delete-player", deletePlayer);
-  //console.log(socket.id, socket.eventNames());
 };
